2f06a10f13ad81dbd5777c1dc5fa6d1927673d1e
talha-exe <talha.exe@outlook.com>
2022-04-06 14:18:39 +0500

    Get date as JSON data

--- user-data/src/routes/time.ts
+++ user-data/src/routes/time.ts
@@ -5,9 +5,9 @@ const moment = require("moment");
 
  router.get('/api/users/time', (req,res) => {
     let {date} = req.body;
-    var localDate = moment().format(); 
-    var utcFormat = moment.utc(localDate).format('YYYY-MM-DD HH:mm:ss a');
-    console.log('LocalDate: ', localDate);
+    // var localDate = moment().format(); 
+    var utcFormat = moment.utc(date).format('YYYY-MM-DD HH:mm:ss a');
+    // console.log('LocalDate: ', localDate);
     console.log('utcFormat: ', utcFormat);
     res.status(200).send(utcFormat);
 })
