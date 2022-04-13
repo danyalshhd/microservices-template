cefe4388ef50ffded88d5f497e90c738dd06c371
Abdul Moiz <abdulmoiz.asif@systemsltd.com>
2022-04-13 11:19:25 +0500

    product crud apis done

---
+++ product/src/index.ts
@@ -0,0 +1,46 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@dstransaction/common';
+import { categoryRouter } from './routes/category';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+    name: 'session',
+  })
+);
+
+app.use(categoryRouter);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
+  try {
+    // await mongoose.connect('mongodb://auth-mongo-srv:27017/product');
+    await mongoose.connect('mongodb://host.docker.internal:27017/product');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
+
+start();
---
+++ product/src/routes/category.ts
@@ -0,0 +1,82 @@
+import { BadRequestError, validateRequest } from '@dstransaction/common';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { Category } from '../models/category';
+const router = express.Router();
+
+router.post(
+  '/api/product/category',
+  [body('name').isString(), body('visible').optional().isBoolean()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { name, visible = true } = req.body;
+    let existingCategory = await Category.findOne({ name });
+    if (existingCategory) {
+      throw new BadRequestError('Category with this name is already present.');
+    }
+    const category = Category.build({ name, visible });
+    await category.save();
+    res.status(201).send(category);
+  }
+);
+
+router.get(
+  '/api/product/category',
+  [body('name').optional().isString(), body('visible').optional().isBoolean()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    try {
+      const { name, visible } = req.body;
+      let queryObj: any = {};
+      name != null && (queryObj.name = name);
+      visible != null && (queryObj.visible = visible);
+      let categories = await Category.find(queryObj);
+      res.send(categories);
+    } catch (error) {
+      throw new BadRequestError('Unable to retrieve Categories.');
+    }
+  }
+);
+
+router.put(
+  '/api/product/category',
+  [
+    body('id').isString(),
+    body('name').optional().isString(),
+    body('visible').optional().isBoolean(),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    try {
+      const { id, name, visible } = req.body;
+      let updateObj: any = {};
+      name != null && (updateObj.name = name);
+      visible != null && (updateObj.visible = visible);
+      const updatedCategory = await Category.findOneAndUpdate(
+        { _id: id },
+        updateObj,
+        { new: true }
+      );
+      res.send(updatedCategory);
+    } catch (error) {
+      throw new BadRequestError('Unable to update Category.');
+    }
+  }
+);
+
+router.delete(
+  '/api/product/category',
+  body('id').isString(),
+  validateRequest,
+  async (req: Request, res: Response) => {
+    try {
+      const { id } = req.body;
+      const deletedCategory = await Category.deleteOne({ _id: id });
+      res.send(deletedCategory);
+    } catch (error) {
+      throw new BadRequestError('Unable to delete Category.');
+    }
+  }
+);
+
+export { router as categoryRouter };
