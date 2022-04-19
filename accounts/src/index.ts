769a14888bda0403a7154381067fa731a9f66674
Danyal <danyalshahid320@gmail.com>
2022-04-19 16:18:06 +0500

    Update index.ts

--- accounts/src/index.ts
+++ accounts/src/index.ts
@@ -5,25 +5,25 @@ import { TransactionCreatedListener } from './events/listeners/transaction-creat
 import { TransactionUpdatedListener } from './events/listeners/transaction-updated-listener';
 
 const start = async () => {
-  if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
-  }
-  if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined');
-  }
-  if (!process.env.NATS_CLIENT_ID) {
-    throw new Error('NATS_CLIENT_ID must be defined');
-  }
-  if (!process.env.NATS_URL) {
-    throw new Error('NATS_URL must be defined');
-  }
-  if (!process.env.NATS_CLUSTER_ID) {
-    throw new Error('NATS_CLUSTER_ID must be defined');
-  }
+//   if (!process.env.JWT_KEY) {
+//     throw new Error('JWT_KEY must be defined');
+//   }
+//   if (!process.env.MONGO_URI) {
+//     throw new Error('MONGO_URI must be defined');
+//   }
+//   if (!process.env.NATS_CLIENT_ID) {
+//     throw new Error('NATS_CLIENT_ID must be defined');
+//   }
+//   if (!process.env.NATS_URL) {
+//     throw new Error('NATS_URL must be defined');
+//   }
+//   if (!process.env.NATS_CLUSTER_ID) {
+//     throw new Error('NATS_CLUSTER_ID must be defined');
+//   }
 
   try {
     console.log(process.env.NATS_CLUSTER_ID);
-    await natsWrapper.connect(process.env.NATS_CLUSTER_ID.trim(), randomBytes(4).toString('hex'), process.env.NATS_URL);
+    await natsWrapper.connect('digicel', randomBytes(4).toString('hex'), 'nats://localhost:4222');
 
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed');
@@ -35,8 +35,8 @@ const start = async () => {
     new TransactionCreatedListener(natsWrapper.client).listen();
     new TransactionUpdatedListener(natsWrapper.client).listen();
 
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to MongoDb');
+//     await mongoose.connect(process.env.MONGO_URI);
+//     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
   }
