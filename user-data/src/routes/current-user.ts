a1805cd32a97a55b5396fea61858f38f6eb70d7e
talha-exe <talha.exe@outlook.com>
2022-04-22 14:53:36 +0500

    Update

--- user-data/src/routes/current-user.ts
+++ user-data/src/routes/current-user.ts
@@ -2,12 +2,11 @@ const express = require("express");
 const router = express.Router();
 const AmazonCognitoIdentity = require("amazon-cognito-identity-js");
 const { poolData } = require('../config/cognito-config');
-const validator = require('../middleware/aws_authourization')
 const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+import { errorHandler, NotFoundError, currentUser , requireAuth } from '@dstransaction/common';
 
 
-
-router.get('/api/users/currentuser', validator.default(), (req: any, res: any) => {
+router.get('/api/users/currentuser', currentUser,requireAuth, (req: any, res: any) => {
   res.send({ currentUser: userPool.getCurrentUser() || null });
 });
 
