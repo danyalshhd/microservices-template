3197bff3a88a4f477f9df43bd95a55ea67dc14cd
Mohsin Shabbir <37940934+MohsinShabbir5@users.noreply.github.com>
2022-04-29 10:09:21 +0500

    http request type changed

--- transactions/src/routes/index.ts
+++ transactions/src/routes/index.ts
@@ -2,6 +2,6 @@ import express from 'express';
 import transactionsController from '../controllers/Transactions'
 const router = express.Router();
 
-router.get('/api/transactions', transactionsController);
+router.post('/api/transactions', transactionsController);
 
 export { router as indexTransactionRouter };
